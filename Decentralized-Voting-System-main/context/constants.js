5aaadb42d9334c36fc7499ddcbd8c5ababe8b6e4
Anurag7645 <114188019+Anurag7645@users.noreply.github.com>
2024-07-16 19:49:43 +0530

    modified:   Decentralized-Voting-System-main/context/constants.js 	modified:   Decentralized-Voting-System-main/package-lock.json 	modified:   Decentralized-Voting-System-main/package.json

---
+++ Decentralized-Voting-System-main/context/constants.js
@@ -0,0 +1,6 @@
+//FbDB2315678afecb367f032d93F642f64180aa3
+
+import voting from './Create.json';
+
+export const VotingAddrss = 'FbDB2315678afecb367f032d93F642f64180aa3';
+export const VotingAddressABI = voting.abi;
